283be6c390e4496a8fb6bf1e1b184efe207ad2cd
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-10-28 11:39:39 +0200

    Added ability to view where you won

--- power4.c
+++ power4.c
@@ -11,11 +11,11 @@ typedef enum {BLACK = 0, RED, GREEN, MAX_COLORS} ColorCode;
 ColorCode ** createGrid (int, int);
 void destroyGrid (ColorCode ***, int);
 void displayColor (int, int, char);
-void displayGrid (ColorCode **, int);
+void displayGrid (ColorCode **, int, int, int, char);
 void play (int);
 void insert(ColorCode**, ColorCode, int, int);
 int lineIsFull(ColorCode**, int );
-int winDetection(ColorCode**, int);
+int winDetection(ColorCode**, int, int*, int*, char*);
 int isFull(ColorCode**, int);
 
 
@@ -59,18 +59,91 @@ void destroyGrid (ColorCode *** M, int n){//n is the matrix size (n*n)
 }
 
 
-void displayGrid (ColorCode ** M, int n){
-    printf("\n");
+void displayGrid (ColorCode ** M, int n, int winline, int wincol, char windir){//'u' : up ; 'd' : down ; 'l' : left ; 'r' : right ;       '1' : left upper diag ; '2' : right upper diag ; '3' : left bottom diag ; '4' : right bottom diag
+  int win = (winline != -1 && wincol != -1 && windir != 0);
+  int x1, y1, x2, y2, x3, y3, x4, y4;//calcul the coordinates of win
 
+  if(win){
+    x1 = winline;
+    y1 = wincol;
+
+    if(windir == 'l'){
+      x2 = x3 = x4 = x1;
+      y2 = y1 -1;
+      y3 = y2 -1;
+      y4 = y3 -1;
+    }else if(windir == 'd'){
+      x2 = x3 = x4 = x1;
+      y2 = y1 +1;
+      y3 = y2 +1;
+      y4 = y3 +1;
+    }else if(windir == 'u'){
+      y2 = y3 = y4 = y1;
+      x2 = x1 -1;
+      x3 = x2 -1;
+      x4 = x3 -1;
+    }else if(windir == 'r'){
+      y2 = y3 = y4 = y1;
+      x2 = x1 +1;
+      x3 = x2 +1;
+      x4 = x3 +1;
+    }else if(windir == '1'){
+      x2 = x1 -1;
+      x3 = x2 -1;
+      x4 = x3 -1;
+      y2 = y1 -1;
+      y3 = y2 -1;
+      y4 = y3 -1;
+    }else if(windir == '2'){
+      x2 = x1 +1;
+      x3 = x2 +1;
+      x4 = x3 +1;
+      y2 = y1 -1;
+      y3 = y2 -1;
+      y4 = y3 -1;
+    }else if(windir == '3'){
+      x2 = x1 -1;
+      x3 = x2 -1;
+      x4 = x3 -1;
+      y2 = y1 +1;
+      y3 = y2 +1;
+      y4 = y3 +1;
+    }else if(windir == '4'){
+      x2 = x1 +1;
+      x3 = x2 +1;
+      x4 = x3 +1;
+      y2 = y1 +1;
+      y3 = y2 +1;
+      y4 = y3 +1;
+    }
+    printf("(%d, %d)\n", x1, y1);
+    printf("(%d, %d)\n", x2, y2);
+    printf("(%d, %d)\n", x3, y3);
+    printf("(%d, %d)\n", x4, y4);
+  }//here (x1, y1), (x2, y2), (x3, y3), (x4, y4) are the 4 wining coordinates
+
+
+
+
+
+    printf("\n");
   for(char i = 'a' ; i <'a'+ n ; i++){    //controls
       printf("|%c ", i);
   }
-  printf("\n");
+  printf("|\n");
 
   for(int i = 0 ; i < n ; i++){
     for(int j = 0 ; j < n ; j++){
       printf("|");
-      displayColor(M[i][j], M[i][j], ' ');  //grid
+      if(!win){
+        displayColor(M[i][j], M[i][j], ' ');  //grid
+      }else{//win case
+        if((i == x1 && j == y1) || (i == x2 && j == y2) || (i == x3 && j == y3) || (i == x4 && j == y4)){
+          displayColor(BLACK, M[i][j], 'X');  //grid
+        }else{
+          displayColor(M[i][j], M[i][j], ' ');  //grid
+        }
+      }
     }
     printf("|\n");
   }
@@ -81,7 +154,7 @@ void insert(ColorCode**Mplay, ColorCode playercolor, int position, int size){
   int level = -1;
   while(level < size-1 && Mplay[level+1][position] == BLACK)
     level++;
-    
+
   Mplay[level][position] = playercolor;
 }
 
@@ -91,7 +164,7 @@ int lineIsFull(ColorCode**Mplay, int pos){
   return 0;
 }
 
-int winDetection(ColorCode**m, int size){
+int winDetection(ColorCode**m, int size, int*winline, int*wincol, char*direction){
   int win = 0;
   ColorCode col = 1;
   int up = 1;
@@ -103,7 +176,7 @@ int winDetection(ColorCode**m, int size){
   int dl = 1;
   int dr = 1;//down right
   while(col < MAX_COLORS && !win){
-    printf(" detection of win for player %d\n", col);
+    //printf(" detection of win for player %d\n", col);
     for(int i = size -1 ; i >= 0 ; i--){//starting from bottom for less calculs if win
       for(int j = 0 ; j < size ; j++){
         if(m[i][j] == col){
@@ -115,7 +188,7 @@ int winDetection(ColorCode**m, int size){
           ur = 1;
           dl = 1;
           dr = 1;//down right
-          printf(" origin (%d, %d)\n", i, j);
+        //printf(" origin (%d, %d)\n", i, j);
           for(int t = 1 ; t < 4 && (up || down || right || left || ul || ur || dl || dr) ; t++){//tests
             up = up && (i - t >= 0) && m[i-t][j] == col;
             down = down && (i + t < size) && m[i+t][j] == col;
@@ -125,12 +198,46 @@ int winDetection(ColorCode**m, int size){
             ur = ur && (i - t >= 0) && (j + t < size) && m[i-t][j+t] == col;
             dl = dl && (i + t < size) && (j - t >= 0) && m[i+t][j-t] == col;
             dr = dr && (i + t < size) && (j + t < size) && m[i+t][j+t] == col;
-            printf("up : %d at iteration %d\n", up, t);
+          //  printf("up : %d at iteration %d\n", up, t);
           }
           win = (up || down || right || left || ul || ur || dl || dr);
-          printf(" end of test : %d",win);
-          if(win)
-            return col; //here the boolean expressions are still avaible
+        //  printf(" end of test : %d",win);
+          if(win){
+            *winline = i;
+            *wincol = j;
+            if(up){
+              *direction = 'u';
+              return col;
+            }
+            if(right){
+              *direction = 'r';
+              return col;
+            }
+            if(down){
+              *direction = 'd';
+              return col;
+            }
+            if(left){
+              *direction = 'l';
+              return col;
+            }
+            if(ul){
+              *direction = '1';
+              return col;
+            }
+            if(ur){
+              *direction = '3';
+              return col;
+            }
+            if(dl){
+              *direction = '3';
+              return col;
+            }
+            if(dr){
+              *direction = '4';
+              return col;
+            }
+          }
         }
       }
     }
@@ -143,38 +250,83 @@ int winDetection(ColorCode**m, int size){
 
 
 void play(int size){
+  int winline = -1;
+  int wincol = -1;
+  char windir = 0;//'u' : up ; 'd' : down ; 'l' : left ; 'r' : right ;       '1' : left upper diag ; '2' : right upper diag ; '3' : left bottom diag ; '4' : right bottom diag
   ColorCode ** Mplay = createGrid(size, 0);
-  displayGrid(Mplay, size);
+  displayGrid(Mplay, size, winline, wincol, windir);
   ColorCode player = 1;
   char res;
-  
+
   while(1){//infinite loop
-    
+
     printf("player %d enter your position : ", player);
     do{
       scanf("%c", &res);
     }while(res < 'a' || res > 'a' + size - 1 || lineIsFull(Mplay, res - 'a'));
-    
+
     insert(Mplay, player, res - 'a', size);
-    
+
     if(player == 1)
       player = 2;
     else
-      player = 1;
-    
-    int winner  = winDetection(Mplay, size);
-    displayGrid(Mplay, size);
+      player = 1;    displayGrid(Mplay, size, winline, wincol, windir);
+
+
+    int winner  = winDetection(Mplay, size, &winline, &wincol, &windir);
+    displayGrid(Mplay, size, winline, wincol, windir);
     if(winner != 0){
       printf("\n\nplayer %d won\n\n" , winner);
       return;
     }
-    
+
   }
   destroyGrid(&Mplay, size);
 }
 
 
-int main(){
-    play(12);
+void randPlay(int size){
+  int winline = -1;
+  int wincol = -1;
+  char windir = 0;//'u' : up ; 'd' : down ; 'l' : left ; 'r' : right ;       '1' : left upper diag ; '2' : right upper diag ; '3' : left bottom diag ; '4' : right bottom diag
+  srand(clock());
+  ColorCode ** Mplay = createGrid(size, 0);
+  displayGrid(Mplay, size, winline, wincol, windir);
+  ColorCode player = 1;
+  char res;
+
+  while(1){//infinite loop
+
+
+    res = (int)rand()% size;
+
+    insert(Mplay, player, res, size);
+
+    if(player == 1)
+      player = 2;
+    else
+      player = 1;
+
+    int winner  = winDetection(Mplay, size, &winline, &wincol, &windir);
+    displayGrid(Mplay, size, winline, wincol, windir);
+    if(winner != 0){
+      printf("\n\nplayer %d won\n\n" , winner);
+      printf("win motif : (%d, %d) %c\n\n",winline, wincol, windir );
+      displayGrid(Mplay, size, winline, wincol, windir);
+      return;
+    }
+
+  }
+  destroyGrid(&Mplay, size);
+
+
+
+
+
+}
+
+
+int main(){/* gcc -c -Wall -Wextra power4.c && gcc power4.o -lm -o power4 && ./power4 */
+    randPlay(12);
     return 0;
 }
